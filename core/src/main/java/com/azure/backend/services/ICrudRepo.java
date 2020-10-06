d18ac522a31034bde5698c83bb0e05cd224115ad
Heba Elayoty <31887807+helayoty@users.noreply.github.com>
2020-10-05 17:07:40 -0700

    Update ICrudRepo.java

--- core/src/main/java/com/azure/backend/services/ICrudRepo.java
+++ core/src/main/java/com/azure/backend/services/ICrudRepo.java
@@ -8,25 +8,4 @@ import reactor.core.publisher.Mono;
 
 @NoRepositoryBean
 public interface ICrudRepo<BaseDTO, String> extends ReactiveCosmosRepository<BaseDTO, String> {
-
-    /**
-     * Common template method used to execute and retrieve queries for the given type using the passed
-     * in query.
-     */
-    <T> Flux<T> getAll(Class<T> classType, SqlQuerySpec sqlQuerySpec);
-
-    Mono<Void> queryDocuments();
-
-    /**
-     * Read the document only if it has been changed, utilizing an ETag check.
-     * @return
-     */
-    Mono<Void> readDocumentOnlyIfChanged();
-
-    /**
-     * Replace the document employing optimistic concurrency using ETag.
-     * @return
-     */
-    Mono<Void> replaceDocumentWithConditionalEtagCheck();
-
 }
