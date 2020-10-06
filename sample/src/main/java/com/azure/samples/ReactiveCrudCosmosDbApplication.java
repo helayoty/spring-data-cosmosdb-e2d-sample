2ebdbec917496114b7a60ae2ddc2cacd7bb81772
Heba Elayoty <31887807+helayoty@users.noreply.github.com>
2020-10-05 17:14:38 -0700

    Update ReactiveCrudCosmosDbApplication.java

--- sample/src/main/java/com/azure/samples/ReactiveCrudCosmosDbApplication.java
+++ sample/src/main/java/com/azure/samples/ReactiveCrudCosmosDbApplication.java
@@ -2,25 +2,23 @@ package com.azure.samples;
 
 import com.azure.samples.models.User;
 import com.azure.samples.repository.ReactiveUserRepository;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @Slf4j
+@ComponentScan(basePackages = {"com.core", "com.azure"})
 public class  ReactiveCrudCosmosDbApplication implements CommandLineRunner {
 
     @Autowired
     private ReactiveUserRepository reactiveUserRepository;
-
-    @Autowired
-    public ReactiveCrudCosmosDbApplication(ReactiveUserRepository reactiveUserRepository) {
-        this.reactiveUserRepository = reactiveUserRepository;
-
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveCrudCosmosDbApplication.class, args);
@@ -32,18 +30,33 @@ public class  ReactiveCrudCosmosDbApplication implements CommandLineRunner {
         User testUser1 = new User("testId1", "testFirstName", "testLastName1");
         User testUser2 = new User("testId2", "testFirstName", "testLastName2");
 
-        Flux<User> users = Flux.just(testUser1,testUser2);
+        reactiveUserRepository.deleteAll();
 
-        reactiveUserRepository.save(testUser1);
-        reactiveUserRepository.save(testUser2);
+        reactiveUserRepository.save(testUser1).flatMap(
+                user -> {
+                    log.info("Saving user : {}",  user);
+                    return Mono.just(user);
+                }).subscribe();
 
+        reactiveUserRepository.save(testUser2).flatMap(
+                user -> {
+                    log.info("Saving user : {}",  user);
+                    return Mono.just(user);
+                }).subscribe();
 
-        reactiveUserRepository.findByFirstName(testUser1.getFirstName());
-        log.info(reactiveUserRepository.findByFirstName(testUser1.getFirstName()).toString());
+        Flux<User> users = reactiveUserRepository.findByFirstName("testFirstName");
+        users.map(u -> {
+            log.info("findByFirstName() : {}", u);
+            return u;
+        }).subscribe();
 
-       //clean-up
-        reactiveUserRepository.delete(testUser1);
-        reactiveUserRepository.delete(testUser2);
+        Flux<User> finalUser = reactiveUserRepository.getUserWithIdLastName(
+                testUser1.getId(),testUser1.getLastName());
+
+        finalUser.map(u -> {
+            log.info("getUserWithIdLastName(): {}" , u);
+            return u;
+        }).subscribe();
 
     }
 
